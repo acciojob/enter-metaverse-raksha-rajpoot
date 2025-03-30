4e459163c731e5d6fa449e9009f4a107c7a26e71
raksha-rajpoot <rakraj654@gmail.com>
2025-03-30 17:13:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+  document.getElementById("enterBtn").addEventListener("click", function() {
+            let statusParagraph = document.getElementById("status");
+            statusParagraph.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+        });
