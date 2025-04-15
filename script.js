c972de0cc153555b082241b4bac4107d271d9d77
raksha-rajpoot <rakraj654@gmail.com>
2025-04-15 18:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //your JS code here. If required.
-  document.getElementById("enterBtn").addEventListener("click", function() {
-            let statusParagraph = document.getElementById("status");
-            statusParagraph.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
-        });
+  // document.getElementById("enterBtn").addEventListener("click", function() {
+  //           let statusParagraph = document.getElementById("status");
+  //           statusParagraph.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+  //       });
+
+const statusPara = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+
+enterButton.addEventListener("click", () => {
+  statusPara.outerHTML = `<h1 id="status">Entered Metaverse</h1>`;
+});
